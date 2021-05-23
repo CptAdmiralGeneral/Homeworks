4256989bb5c2c121e9ff600d6d9ca28f8c7332fa
CptAdmiralGeneral <void405@gmail.com>
2021-05-23 22:47:53 +0300

    Done. Features to add: Exceptions catchers. Split name, surname. Split name and work Experience of trainers and masseurs Add "Sout" for easy navigation

--- src/popovskyi/valentyn/Homework8Classes/Main.java
+++ src/popovskyi/valentyn/Homework8Classes/Main.java
@@ -3,40 +3,109 @@ package popovskyi.valentyn.Homework8Classes;
 import java.util.Scanner;
 
 public class Main {
+    static Administrator administrator = new Administrator();
 
     public static void main(String[] args) {
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            int t = scanner.nextInt();
-            System.out.println("Choose category: 1-Worker or 2-Client");
-            //Worker
-            if (t == 1) {
-                System.out.println("Choose position: 1-Administrator, 2-Trainer, 3-Masseur");
-                t = scanner.nextInt();
-                //Administrator
-                if (t == 1) {
-                    System.out.println("Choose option: 1-Create new Administrator, 2-Add new Client, 3-Delete Client");
-                    t = scanner.nextInt();
-                }
-                //Trainer
-                if (t == 2) {
-                    System.out.println("Choose option: 1-Create new Trainer, 2-Change client's quantity of trainings, 3-Show Trainers");
-                    t = scanner.nextInt();
-                }
-                //Masseur
-                if (t == 3) {
-                    System.out.println("Choose option: 1-Create new Masseur, 2-Change client's quantity of massage, 3-Show Masseurs");
-                    t = scanner.nextInt();
-                }
-            }
-            //Client
-            if (t == 2) {
-                System.out.println("Choose option: 1-Create new Client, 2-Show Clients");
-                t = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (isExitEntered(input)) break;
+            String nextInput = scanner.nextLine();
+            if (input.equals("add client to trainer")) {
+                addClientToTrainer(nextInput);
+            }
+            if (input.equals("add client to masseur")) {
+                addClientToMasseur(nextInput);
+            }
+            if (input.equals("add trainer")) {
+                addTrainer(nextInput);
+            }
+            if (input.equals("add masseur")) {
+                addMasseur(nextInput);
+            }
+            if (input.equals("remove trainer")) {
+                removeTrainer(nextInput);
+            }
+            if (input.equals("remove masseur")) {
+                removeMasseur(nextInput);
+            }
+            if (input.equals("remove client from trainer")) {
+                removeClientFromTrainer(nextInput);
+            }
+            if (input.equals("remove client from masseur")) {
+                removeClientFromMasseur(nextInput);
+            }
+            if (input.equals("add training to client")) {
+                addTrainingAmount(nextInput);
+            }
+            if (input.equals("add massage to client")) {
+                addMassageAmount(nextInput);
             }
         }
-
-
     }
+
+
+    static void addClientToTrainer(String input) {
+        String[] array = input.split(",");
+        if (administrator.addClientToTrainer(array[0], array[1]) < 0) {
+            System.out.println("Trainer not found");
+        }
+    }
+
+    static void addClientToMasseur(String input) {
+        String[] array = input.split(",");
+        if (administrator.addClientToMasseur(array[0], array[1]) < 0) {
+            System.out.println("Masseur not found");
+        }
+    }
+
+    static void addTrainer(String input) {
+        String[] array = input.split(",");
+        administrator.addTrainer(array[0], Integer.parseInt(array[1]));
+    }
+
+    static void removeTrainer(String input) {
+        String[] array = input.split(",");
+        administrator.removeTrainer(array[0], Integer.parseInt(array[1]));
+    }
+
+    static void addMasseur(String input) {
+        String[] array = input.split(",");
+        administrator.addMasseur(array[0], Integer.parseInt(array[1]));
+    }
+
+    static void removeMasseur(String input) {
+        String[] array = input.split(",");
+        administrator.removeMasseur(array[0], Integer.parseInt(array[1]));
+    }
+
+    static void removeClientFromTrainer(String input) {
+        String[] array = input.split(",");
+        administrator.removeClientFromTrainer(array[0], array[1]);
+    }
+
+    static void removeClientFromMasseur(String input) {
+        String[] array = input.split(",");
+        administrator.removeClientFromMasseur(array[0], array[1]);
+    }
+
+    static void addTrainingAmount(String input) {
+        String[] array = input.split(",");
+        administrator.addClientTrainings(array[0], array[1], Integer.parseInt(array[2]));
+    }
+
+    static void addMassageAmount(String input) {
+        String[] array = input.split(",");
+        administrator.addClientMassages(array[0], array[1], Integer.parseInt(array[2]));
+    }
+
+    static boolean isExitEntered(String input) {
+        return input.equals("end");
+    }
+
+
 }
+
+
+
